19f010d2f9d342fbfa84fc3111498dcae2f20cf5
Hanno <hanno.van.der.maas@hotmail.com>
2025-02-05 07:15:00 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMembers() {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/members/skills-members.html',
+        controller: 'SkillsMembersCtrl',
+        controllerAs: 'skillsMembersCtrl'
+    };
+}
